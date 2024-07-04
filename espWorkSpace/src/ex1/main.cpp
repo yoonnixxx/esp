4689ff21b5ff046c14655964183647006d0a2e2c
yoonnixxx <pastuch@alunos.utfpr.edu.br>
2024-07-04 08:36:38 -0300

    dual core

--- espWorkSpace/src/ex1/main.cpp
+++ espWorkSpace/src/ex1/main.cpp
@@ -1,19 +1,29 @@
 #include <Arduino.h>
 
 #define LEDPIN 2
-int delaytime = 2000;
+int delaytime = 1000;
+void Para(void *parametro){
+    Serial.println("Led acesso");
+ 
+    Serial.println("Led apagado");
 
+    }
 void setup(){
     Serial.begin(115200);
-    pinMode(LEDPIN, OUTPUT);
+    //pinMode(LEDPIN, OUTPUT);
+    xTaskCreatePinnedToCore(
+    Para,
+    "Procura",
+    10000,
+    NULL,
+    1,
+    NULL,
+    1
+  );
 }
 
 void loop(){
-    digitalWrite (LEDPIN, HIGH);
-    Serial.println("Led acesso");
-    delay (delaytime);
-
-    digitalWrite (LEDPIN, LOW);
-    Serial.println("Led apagado");
-    delay (delaytime);
+    Serial.println("");
+    Serial.println("O");
+    Serial.println("");
 }
